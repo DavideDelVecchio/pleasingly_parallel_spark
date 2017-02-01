be482b2f18bc6684c1786f2853842b9835cbcf33
Samuel Kemp <samuel_kemp@outlook.com>
2017-02-01 18:02:57 +0000

    core utlization and cluster details function

--- 00_spark_cc.R
+++ 00_spark_cc.R
@@ -1,6 +1,27 @@
+### PARAMETERS - CHANGE TO SUIT
+# We utilize 50% of the cluster cores - you can change this
+coreUtilization <- 0.50
+# change the namenode address
 myNameNode <- "<dns_name_of_head_node>"
-numberOfWorkerNodes <- 10 # number of worker nodes in cluster
-numberOfClusterCores <- 160 # total number of (physical) cores on data nodes you wish to use
+
+
+
+# This function finds the number of nodes and cores in the cluster
+rxGetClusterDetails <- function()
+{
+  xx <- system2("yarn", c("node -list -showDetails"), stderr=FALSE, stdout = TRUE)
+  xx <- xx[grep("vCores",xx)]
+  xx <- do.call("rbind",strsplit(xx,"vCores:"))[,2]
+  xx <- as.integer(gsub(">","",xx))
+  clusterDetails <- xx[seq(1, length(xx), by=2)]
+  nNodes <- length(clusterDetails)
+  clusterCores <- sum((clusterDetails+1)/2)
+  return(list(nNodes=nNodes, totalClusterCores=clusterCores, coresByNode=(clusterDetails+1)/2))
+}
+
+clusterDetails <- rxGetClusterDetails()
+numberOfWorkerNodes <- clusterDetails$nNodes # number of worker nodes in cluster
+numberOfClusterCores <- floor(clusterDetails$totalClusterCores * coreUtilization) # total number of (physical) cores on data nodes you wish to use
 mySparkCluster <- RxSpark(nameNode = myNameNode,
                           consoleOutput = TRUE,
                           numExecutors = numberOfWorkerNodes,
